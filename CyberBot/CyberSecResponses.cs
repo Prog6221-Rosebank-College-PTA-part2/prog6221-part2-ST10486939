f054356337b9efb9c9acc4ee0b1a57562dd1ca34
ST10486939 <ST10486939@rcconnect.edu.za>
2026-05-26 23:00:50 +0200

    The basic file structure for the CyberSecurity Chatbot that includes MainWindow.xaml with the basic GUI window and other files that will hold the chatbot code. Also included are the ASCII art logo and the audio greeting file.

---
+++ CyberBot/CyberSecResponses.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CyberBot
+{
+    public class CyberSecResponses
+    {
+
+    }
+}
---
+++ CyberBot/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using CyberBot;
+using System.IO;
+using System.Media;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CyberBotWPF
+{
+    public partial class MainWindow : Window
+    {
+        private CSChatBot bot = new CSChatBot();
+        public MainWindow()
+        {
+            InitializeComponent();
+            Logo();
+
+            Audio();
+ 
+
+        }
+
+        //ASCII ART
+        private void Logo()
+        {
+            try
+            {
+                string logo = File.ReadAllText("ascii-text-art.txt");
+                LogoTextBox.Text = logo;
+            }
+            catch
+            {
+                LogoTextBox.Text = "CYBERBOT";
+            }
+        }
+
+        //AUDIO GREETING
+        private void Audio()
+        {
+            try
+            {
+                SoundPlayer player = new SoundPlayer("cyberbotGreeting.wav");
+                player.Play();
+            }
+            catch
+            {
+
+            }
+        }
+
+        //SEND BUTTON
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        //ENTER KEY SUPPORT
+        private void MessageTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+    }
+
+}
