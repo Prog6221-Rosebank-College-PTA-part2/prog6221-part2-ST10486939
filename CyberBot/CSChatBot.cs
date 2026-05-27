9acc399fc6ebce79a6b090d6b5535d8a2df61389
ST10486939 <ST10486939@rcconnect.edu.za>
2026-05-27 23:19:09 +0200

    The basic code for the chatbot has been added. The chatbot can now take the name of the user and store it to use it late. The user interface has also been updated. the colour has been changed and the text bubbles have been customized. The ascii art is also displayed as the header to the chatbot.

--- CyberBot/CSChatBot.cs
+++ CyberBot/CSChatBot.cs
@@ -1,11 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace CyberBot
 {
-   public class CSChatBot
+    public class CSChatBot
     {
+        
 
+        //RANDOM OBJECT
+        private Random random = new Random();
+
+        //CYBERBOT RESPONSES 
+        public string GetResponse(string userInput, string userName)
+        {
+            userInput = userInput.ToLower().Trim();
+
+            //CHATBOT GREETING
+            if(userInput.Contains("hello") || userInput.Contains("hi"))
+            {
+                return "Hello " + userName + "! How can I help you stay safe online?";
+            }
+
+            //GENERIC QUESTIONS
+            else if(userInput.Contains("how are you"))
+            {
+                return "I am excited to teach you how to be safe online";
+            }
+
+            else if(userInput.Contains("what is your purpose"))
+            {
+                return "My purpose is to teach you about cybersecurity so you can stay safe whilst online.";
+            }
+
+            //PASSWORD SAFETY
+            else if (userInput.Contains("password"))
+            {
+                return userName + ", your password should never be easily guessable. A strong password has at least 12 characters " +
+                    "that includes upper and lowercase letters, numbers and special symbols.";
+            }
+
+
+            //PHISHING
+            else if(userInput.Contains("phishing"))
+            {
+                return "Phishing is a cyber attck that uses fraudulent emails, text messages, phone calls, or websites to trick people " +
+                    "into sharing sensitive information.";
+            }
+
+            //SAFE BROWSING
+            else if(userInput.Contains("browsing"))
+            {
+                return "Safe browsing starts with awareness. " + userName + ", use a strong unique password for every account. Be cautious" +
+                    " with links and attchments. Pay attention to websites credibility.";
+            }
+
+            //GOODBYE
+            else if (userInput.Contains("bye"))
+            {
+                return "Goodbye " + userName + "! I hope you stay safe online.";
+            }
+
+            else
+            {
+                return "I'm not sure about that. Try asking another question related to cybersecurity or type 'bye' to end our conversation.";
+            }
+        }
     }
 }
--- CyberBot/MainWindow.xaml.cs
+++ CyberBot/MainWindow.xaml.cs
@@ -1,32 +1,57 @@
 ﻿using CyberBot;
+using System.Drawing;
 using System.IO;
 using System.Media;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace CyberBotWPF
 {
     public partial class MainWindow : Window
     {
         private CSChatBot bot = new CSChatBot();
+        private string userName = "";
+        
         public MainWindow()
         {
             InitializeComponent();
             Logo();
 
             Audio();
- 
+
+            BotMessage("Welcome to the Cyber Security Awareness chatbot");
+            BotMessage("What is your name?");
+            
 
         }
 
         //ASCII ART
-        private void Logo()
+        private async Task Logo()
         {
             try
             {
                 string logo = File.ReadAllText("ascii-text-art.txt");
                 LogoTextBox.Text = logo;
+
+                //flicker effect
+                for (int i=0; i<6; i++)
+                {
+                    LogoTextBox.Text = "";
+
+                    await Task.Delay(80);
+
+                    LogoTextBox.Text = logo;
+
+                    await Task.Delay(80);
+                }
+
+                //stable no flicker art
+                LogoTextBox.Text = logo;
+
             }
             catch
             {
@@ -51,13 +76,159 @@ namespace CyberBotWPF
         //SEND BUTTON
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            SendMessage();
+        }
+
+        //SEND MESSAGE 
+        private async void SendMessage()
+        {
+            string userInput = MessageTextBox.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(userInput))
+                return;
+
+            UserMessage(userInput);
+
+            MessageTextBox.Clear();
+
+            //USERNAME INPUT
+            if (string.IsNullOrEmpty(userName))
+            {
+                userName = userInput;
+
+                //typing bubble
+                Border typingBubble = TypingBubble();
+                ChatPanel.Children.Add(typingBubble);
+                ChatScrollViewer.ScrollToEnd();
+
+                //simulateed delay
+                await Task.Delay(1500);
+
+                //remove typing bubble
+                ChatPanel.Children.Remove(typingBubble);
+
+                BotMessage("Nice to meet you, " + userName + "!");
+                BotMessage("Ask me anything about cybersecurity.");
+
+                return;
+            }
+
+            //TYPING ANIMATION
+            Border bottypingBubble = TypingBubble();
+            ChatPanel.Children.Add(bottypingBubble);
+            ChatScrollViewer.ScrollToEnd();
+            await Task.Delay(1500);
+            ChatPanel.Children.Remove(bottypingBubble);
+
+            //Chatbot response
+            string response = bot.GetResponse(userInput, userName);
+
+            BotMessage(response);
 
         }
 
         //ENTER KEY SUPPORT
         private void MessageTextBox_KeyDown(object sender, KeyEventArgs e)
         {
+            
+            if(e.Key == Key.Enter)
+            {
+                SendMessage();
+            }
+        }
 
+
+        //USER INPUT
+        private void UserMessage(string message)
+        {
+            Border bubble = new Border
+            { 
+                Background = System.Windows.Media.Brushes.HotPink,
+                CornerRadius = new CornerRadius(15),
+                Padding = new Thickness(12),
+                Margin = new Thickness(150, 5, 10, 5),
+                HorizontalAlignment = HorizontalAlignment.Right,
+                MaxWidth = 400
+            };
+
+            TextBlock text = new TextBlock
+            {
+                Text = userName+": "+message,
+                Foreground = System.Windows.Media.Brushes.White,
+                TextWrapping = TextWrapping.Wrap,
+                FontSize = 15,
+                TextAlignment = TextAlignment.Left
+            };
+
+            bubble.Child = text;
+
+            ChatPanel.Children.Add(bubble);
+
+            ChatScrollViewer.ScrollToEnd();
+        }
+
+        private object newBrushConverter()
+        {
+            throw new NotImplementedException();
+        }
+
+        //BOT MESSAGE
+        private void BotMessage(string message)
+        {
+            Border bubble = new Border
+            {
+                //Background = System.Windows.Media.Brushes.DarkSlateGray,
+                Background = System.Windows.Media.Brushes.DimGray,
+                CornerRadius = new CornerRadius(15),
+                Padding = new Thickness(12),
+                Margin = new Thickness(10, 5, 150, 5),
+                HorizontalAlignment = HorizontalAlignment.Left,
+                MaxWidth = 400,
+                BorderBrush = System.Windows.Media.Brushes.DimGray,
+                BorderThickness = new Thickness(1)
+            };
+
+            TextBlock text = new TextBlock
+            {
+                Text = "Cyberbot: " + message,
+                Foreground = System.Windows.Media.Brushes.WhiteSmoke,
+                TextWrapping = TextWrapping.Wrap,
+                FontSize = 15
+            };
+
+            bubble.Child = text;
+
+            ChatPanel.Children.Add(bubble);
+
+            ChatScrollViewer.ScrollToEnd();
+        }
+
+        //TYPING BUBBLE
+        private Border TypingBubble()
+        {
+            Border bubble = new Border
+            {
+                Background = System.Windows.Media.Brushes.DimGray,
+                CornerRadius = new CornerRadius(15),
+                Padding = new Thickness(12),
+                Margin = new Thickness(10, 5, 150, 5),
+                HorizontalAlignment = HorizontalAlignment.Left,
+                MaxWidth = 250,
+                BorderBrush = System.Windows.Media.Brushes.DimGray,
+                BorderThickness = new Thickness(1)
+            };
+
+            TextBlock text = new TextBlock
+            {
+                Text = "Cyberbot is typing...",
+                Foreground = System.Windows.Media.Brushes.WhiteSmoke,
+                FontStyle = FontStyles.Italic,
+                FontSize = 14
+            };
+
+            bubble.Child = text;
+
+            return bubble;
         }
 
     }
